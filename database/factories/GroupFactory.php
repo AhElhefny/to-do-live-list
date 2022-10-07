451c8cd2087fee298a15f1e8e55eb6713a968f25
ahmed elhefny <ahmedelhefny2297@gmail.com>
2022-10-07 16:29:22 +0200

    first commit

---
+++ database/factories/GroupFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Group>
+ */
+class GroupFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->slug,
+            'user_id' => $this->faker->numberBetween(1,User::count())
+        ];
+    }
+}
---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Group;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->title,
+            'description' => $this->faker->text,
+            'group_id' => $this->faker->numberBetween(1,Group::count()),
+            'user_id' => $this->faker->numberBetween(1,User::count())
+        ];
+    }
+}
